7d184b8175b58a0e5e3cac8ba15b0fa89168f064
adithyasource <adithyasrc@gmail.com>
2025-06-21 18:26:53 +0530

    more concise wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,8 @@
-local wt = require 'wezterm'
+local wt = require("wezterm")
+
 local config = {
-  window_padding = {
-    left = 2,
-    right = 2,
-    top = 0,
-    bottom = 0,
-  },
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   hide_tab_bar_if_only_one_tab = true,
-  font_size = 13.0,
   initial_cols = 90,
   initial_rows = 30,
   font = wt.font("SimSun-ExtB"),
@@ -19,34 +14,16 @@ local config = {
     cursor_border = "#FFFFFF",
     selection_bg = "#CACACA",
     selection_fg = "#232323",
-    ansi = {
-      "#383838", -- black
-      "#A95551", -- red
-      "#666666", -- green
-      "#A98051", -- yellow
-      "#657D3E", -- blue
-      "#767676", -- magenta
-      "#C9C9C9", -- cyan
-      "#BABABA", -- white
-    },
-    brights = {
-      "#474747", -- bright black
-      "#A97775", -- bright red
-      "#8C8C8C", -- bright green
-      "#A99175", -- bright yellow
-      "#98BD5E", -- bright blue
-      "#A3A3A3", -- bright magenta
-      "#DCDCDC", -- bright cyan
-      "#CACACA", -- bright white
-    },
-  }
+    ansi = { "#383838", "#A95551", "#666666", "#A98051", "#657D3E", "#767676", "#C9C9C9", "#BABABA", },
+    brights = { "#474747", "#A97775", "#8C8C8C", "#A99175", "#98BD5E", "#A3A3A3", "#DCDCDC", "#CACACA", },
+  },
 }
 
 if wt.target_triple:find("apple") then
   config.font_size = 20
 elseif wt.target_triple:find("windows") then
+  config.font_size = 13
   config.default_prog = { "powershell.exe", "-NoLogo" }
 end
 
 return config
-
