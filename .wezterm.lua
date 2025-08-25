7ce8abd477284f76f5e47e713303fcf2d70602c9
adithyasource <adithyasrc@gmail.com>
2025-08-25 16:31:51 +0530

    feat: shorter wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,31 +1,15 @@
 local wt = require("wezterm")
 
-local config = {
+wt.on('format-window-title', function () return 'terminal' end)
+local platformIs = function(s) return wt.target_triple:find(s) end
+
+return {
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   hide_tab_bar_if_only_one_tab = true,
   initial_cols = 90,
   initial_rows = 30,
+  color_scheme = "Dehydration (Gogh)",
   font = wt.font("SimSun-ExtB"),
-  colors = {
-    foreground = "#A0A0A0",
-    background = "#232323",
-    cursor_bg = "#FFFFFF",
-    cursor_fg = "#232323",
-    cursor_border = "#FFFFFF",
-    selection_bg = "#CACACA",
-    selection_fg = "#232323",
-    ansi = { "#383838", "#9b6e6b", "#777777", "#9b806b", "#778060", "#767676", "#b8b8b8", "#b0b0b0", },
-    brights = { "#474747", "#9b7f7d", "#8c8c8c", "#9b8c7d", "#a0b77d", "#a3a3a3", "#cfcfcf", "#c0c0c0", },
-  }
+  font_size = platformIs("windows") and 13 or 20,
+  default_prog = platformIs("windows") and { "powershell.exe", "-NoLogo" },
 }
-
-if wt.target_triple:find("apple") then
-  config.font_size = 20
-elseif wt.target_triple:find("windows") then
-  config.font_size = 13
-  config.default_prog = { "powershell.exe", "-NoLogo" }
-end
-
-wt.on('format-window-title', function () return 'terminal' end)
-
-return config
