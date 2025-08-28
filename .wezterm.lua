dc553eb99cf1b21ac59f7f28dca373da07c2afd2
adithyasource <adithyasrc@gmail.com>
2025-08-29 00:34:29 +0530

    mpv moved to lua and wezterm compressed more

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,11 @@
-local wt = require("wezterm")
-
-wt.on('format-window-title', function () return 'terminal' end)
-local platformIs = function(s) return wt.target_triple:find(s) end
-
+local w = require("wezterm")
 return {
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   hide_tab_bar_if_only_one_tab = true,
   initial_cols = 90,
   initial_rows = 30,
   color_scheme = "Dehydration (Gogh)",
-  font = wt.font("SimSun-ExtB"),
-  font_size = platformIs("windows") and 13 or 20,
-  default_prog = platformIs("windows") and { "powershell.exe", "-NoLogo" },
+  font = w.font("SimSun-ExtB"),
+  font_size = w.target_triple:find("windows") and 13 or 20,
+  default_prog = w.target_triple:find("windows") and { "powershell.exe", "-NoLogo" },
 }
