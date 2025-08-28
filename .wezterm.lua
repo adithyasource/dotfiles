3623f8589c21f30c475af96d59ca68c14bd76d5f
adithyasource <adithyasrc@gmail.com>
2025-08-29 00:43:32 +0530

    go back to better wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,23 @@
 local w = require("wezterm")
+w.on('format-window-title', function () return 'terminal' end)
 return {
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   hide_tab_bar_if_only_one_tab = true,
   initial_cols = 90,
   initial_rows = 30,
-  color_scheme = "Dehydration (Gogh)",
+  colors = {
+    foreground = "#A0A0A0",
+    background = "#232323",
+    cursor_bg = "#FFFFFF",
+    cursor_fg = "#232323",
+    cursor_border = "#FFFFFF",
+    selection_bg = "#CACACA",
+    selection_fg = "#232323",
+    ansi = { "#383838", "#9b6e6b", "#777777", "#9b806b", "#778060", "#767676", "#b8b8b8", "#b0b0b0", },
+    brights = { "#474747", "#9b7f7d", "#8c8c8c", "#9b8c7d", "#a0b77d", "#a3a3a3", "#cfcfcf", "#c0c0c0", },
+  },
   font = w.font("SimSun-ExtB"),
   font_size = w.target_triple:find("windows") and 13 or 20,
   default_prog = w.target_triple:find("windows") and { "powershell.exe", "-NoLogo" },
 }
+
